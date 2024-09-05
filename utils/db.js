0070705a735ef0cd8b0171ea32d535a1734139d8
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2024-09-05 02:58:24 +0000

    fix eslint redis/db

--- utils/db.js
+++ utils/db.js
@@ -1,56 +1,55 @@
 import { MongoClient } from 'mongodb';
 
 class DBClient {
-    constructor() {
-        const host = process.env.DB_HOST || 'localhost';
-        const port = process.env.DB_PORT || 27017;
-        const database = process.env.DB_DATABASE || 'files_manager';
-        const url = `mongodb://${host}:${port}/${database}`;
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}/${database}`;
 
-        //Mongo client
-        this.client = new MongoClient(url, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-        });
+    // Mongo client
+    this.client = new MongoClient(url, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
 
-        //connect to MongoDB server
-        this.client.connect((err) => {
-            if (err) {
-                console.error(`MongoDB client not connected to the server: ${err}`);
-            }
-        });
-        //succesful connection
-        this.db = this.client.db(`${database}`);
+    // connect to MongoDB server
+    this.client.connect((err) => {
+      if (err) {
+        console.error(`MongoDB client not connected to the server: ${err}`);
+      }
+    });
+    // succesful connection
+    this.db = this.client.db(`${database}`);
+  }
 
-    }
-
-    /**
+  /**
      * Checks if the MongoDB client is alive.
      * @returns {boolean} True if the connection is successful, otherwise false.
      */
-    isAlive() {
-        return this.client.isConnected();
-    }
+  isAlive() {
+    return this.client.isConnected();
+  }
 
-    /**
+  /**
      * Returns the number of documents in the users collection.
      * @returns {Promise<number>} The number of documents in the users collection.
      */
-    async nbUsers() {
-        const users = this.db.collection('users');
-        const usersNum = await users.countDocuments();
-        return usersNum;
-    }
+  async nbUsers() {
+    const users = this.db.collection('users');
+    const usersNum = await users.countDocuments();
+    return usersNum;
+  }
 
-    /**
+  /**
      * Returns the number of documents.
      * @returns {Promise<number>} The number of documents in the files collection.
      */
-    async nbFiles() {
-        const files = this.db.collection('files');
-        const filesNum = await files.countDocuments();
-        return filesNum;
-    }
+  async nbFiles() {
+    const files = this.db.collection('files');
+    const filesNum = await files.countDocuments();
+    return filesNum;
+  }
 }
 // Create and export an instance of DBClient
 const dbClient = new DBClient();
